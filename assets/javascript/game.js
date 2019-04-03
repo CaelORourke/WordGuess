ca72a244bbc23ee9b5593b23bdac2c0500a4911d
Shane <shanegates@hotmail.com>
2019-04-02 20:43:54 -0700

    display wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
 // WHAT TO TRACK
 // a list of words to guess
-// track wrong guesses
 
 // WHAT TO DO
 // TODO: restart the game
 // TODO: check if user won
 // TODO: check is user lost
-// TODO: display the wrong guesses
 // TODO: display the current word
 
 var wins = 0;
@@ -14,6 +12,7 @@ var losses = 0;
 var guessesRemaining = 10;
 var keyPressed = "";
 var currentWord = "shane";
+var wrongGuesses = [];
 
 // useful for debugging
 console.log(currentWord);
@@ -29,26 +28,39 @@ document.getElementById("losses").innerHTML = losses;
 // display the guesses remaining
 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
+// display the wrong guesses
+document.getElementById("wrongGuesses").innerHTML = wrongGuesses;
+
 // listen for letters that players type
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     // console.log(event.keyCode);
     // console.log(event.key);
 
     // NOTE: we only care about letters
-    if (event.keyCode >= 65 && event.keyCode <= 90)
-    {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
         console.log(event.key.toLowerCase());
         keyPressed = event.key.toLowerCase();
 
         // console.log(currentWord.indexOf(keyPressed));
         // check if key pressed is in the current word
-        if (currentWord.indexOf(keyPressed) > -1)
-        {
+        if (currentWord.indexOf(keyPressed) > -1) {
             console.log("correct guess");
         }
-        else
-        {
+        else {
             console.log("wrong guess");
+            wrongGuesses.push(keyPressed);
         }
+
+        // display the wins
+        document.getElementById("wins").innerHTML = wins;
+
+        // display the losses
+        document.getElementById("losses").innerHTML = losses;
+
+        // display the guesses remaining
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+        // display the wrong guesses
+        document.getElementById("wrongGuesses").innerHTML = wrongGuesses;
     }
 }
