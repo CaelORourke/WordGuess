9f81a706ee6a4d7b04b2cac82451da990deda5d7
Shane <shanegates@hotmail.com>
2019-04-02 20:22:25 -0700

    wired up wins, losses, and guesses remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,40 @@
 // WHAT TO TRACK
-// track wins
-// track losses
 // a list of words to guess
 // the current word to guess
 // track wrong guesses
-// track the key the user presses
 
 // WHAT TO DO
-// TODO: use key events to listen for letters that players type
 // TODO: choose a word
 // TODO: restart the game
 // TODO: check if key pressed is in the current word
 // TODO: check if user won
 // TODO: check is user lost
-// TODO: display the wins
-// TODO: display the losses
 // TODO: display the wrong guesses
 // TODO: display the current word
-// TODO: display the guesses remaining
+
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
+var keyPressed = "";
+
+// display the wins
+document.getElementById("wins").innerHTML = wins;
+
+// display the losses
+document.getElementById("losses").innerHTML = losses;
+
+// display the guesses remaining
+document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+// listen for letters that players type
+document.onkeyup = function(event) {
+    // console.log(event.keyCode);
+    // console.log(event.key);
+
+    // NOTE: we only care about letters
+    if (event.keyCode >= 65 && event.keyCode <= 90)
+    {
+        console.log(event.key.toLowerCase());
+        keyPressed = event.key.toLowerCase();
+    }
+}
