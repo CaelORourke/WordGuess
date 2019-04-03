e2b200d68c32561e298d62b5c8190a805739fb65
Shane <shanegates@hotmail.com>
2019-04-02 19:43:15 -0700

    javascript todo list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
+// WHAT TO TRACK
+// track wins
+// track losses
+// a list of words to guess
+// the current word to guess
+// track wrong guesses
+// track the key the user presses
+
+// WHAT TO DO
 // TODO: use key events to listen for letters that players type
-
 // TODO: choose a word
-
 // TODO: restart the game
+// TODO: check if key pressed is in the current word
+// TODO: check if user won
+// TODO: check is user lost
+// TODO: display the wins
+// TODO: display the losses
+// TODO: display the wrong guesses
+// TODO: display the current word
+// TODO: display the guesses remaining
