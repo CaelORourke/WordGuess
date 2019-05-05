0bda773641720a7365fb0c5ba097a1943d7fc67b
Shane <shanegates@hotmail.com>
2019-05-05 12:21:06 -0700

    refactored wordGuess & stopwatch. updated css.

---
+++ assets/js/game.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+    const correctGuessSound = $("#correctGuessSound")[0];
+    const wrongGuessSound = $("#wrongGuessSound")[0];
+
+    function clearDisplay() {
+        $("#currentWord, #guessesRemaining, #lettersGuessed, #currentWordLabel, #lossesLabel, #guessesRemainingLabel, #lettersGuessedLabel").empty();
+    }
+
+    function displayLabels() {
+        $("#currentWordLabel").text("Word to Guess:");
+        $("#guessesRemainingLabel").text("Guesses Remaining:");
+        $("#lettersGuessedLabel").text("Letters Guessed:");
+    }
+
+    function displayStats() {
+        $("#currentWord").html(wordGuess.getLettersToDisplay().join("&nbsp;"));
+        $("#score").text(wordGuess.getScore());
+        $("#guessesRemaining").text(wordGuess.getGuessesRemaining());
+        $("#lettersGuessed").text(wordGuess.getLettersGuessed().join(" "));
+    }
+
+    function newRound() {
+        wordGuess.newRound();
+        displayStats();
+        stopwatch.resetTimer();
+        stopwatch.startTimer();
+    }
+
+    function playSound(sound) {
+        if (sound.paused) {
+            sound.play();
+        }
+        else {
+            sound.pause();
+            sound.currentTime = 0;
+            sound.play();
+        }
+    }
+
+    function resetGame() {
+        wordGuess.resetGame();
+        stopwatch.startTime = 60;//seconds
+        stopwatch.resetTimer();
+        clearDisplay();
+        $("#instructions").text("Press any key to get started!");
+    }
+
+    function showGameOver(message) {
+        $('#gameOverMessage').html(message);
+        $('#gameOverDialog').modal('show');
+    }
+
+    function showQuitOrContinue(title, message) {
+        stopwatch.stopTimer();
+        $('#winOrLossLabel').html(title);
+        $('#winOrLossMessage').html(message);
+        $('#quitOrContinueDialog').modal('show');
+    }
+
+    stopwatch.onDisplayTime((time) => {
+        $("#timerDisplay .card-text").text(time);
+    });
+
+    stopwatch.onTimesUp(() => {
+        showGameOver("Time's up!");
+    });
+
+    $("#continueGameButton").on("click", () => {
+        $('#quitOrContinueDialog').modal('hide');
+        newRound();
+    });
+
+    $("#quitGameButton, #closeButton").on("click", () => {
+        $('#quitOrContinueDialog').modal('hide');
+        resetGame();
+    });
+
+    $("#okButton").on("click", () => {
+        $('#gameOverDialog').modal('hide');
+        resetGame();
+    });
+
+    $("#quitOrContinueDialog, #gameOverDialog").on("hidden.bs.modal", () => {
+        if (wordGuess.getGameStarted() && wordGuess.getGamePaused()) {
+            resetGame();
+        }
+    });
+
+    resetGame();
+
+    $(document).keyup((event) => {
+        if (wordGuess.getGamePaused()) {
+            return;
+        }
+
+        if (wordGuess.getGameStarted()) {
+            if (event.which === 32)//spacebar
+            {
+                stopwatch.stopTimer();
+                showGameOver("You lost!");
+            }
+
+            // NOTE: we only care about letters
+            if (event.which >= 65 && event.which <= 90) {
+
+                let keyPressed = event.key.toLowerCase();
+
+                // don't let the user make the same guess
+                if (wordGuess.hasGuessedLetter(keyPressed)) {
+                    return;
+                }
+
+                if (wordGuess.isLetterInWord(keyPressed, stopwatch.time)) {
+                    playSound(correctGuessSound);
+                }
+                else {
+                    playSound(wrongGuessSound);
+                }
+
+                if (wordGuess.hasUserWon()) {
+                    showQuitOrContinue("Congratulations!", "You won!");
+                }
+
+                if (wordGuess.hasUserLost()) {
+                    stopwatch.stopTimer();
+                    showGameOver("You lost!");
+                }
+
+                displayStats()
+            }
+        }
+        else {
+            $("#instructions").text("Press a letter key to guess. Press spacebar to quit.");
+            displayLabels();
+            newRound();
+        }
+    });
+});
---
+++ assets/js/stopwatch.js
@@ -0,0 +1,72 @@
+const stopwatch = (function () {
+    let displayTime = null;
+    let intervalId = null;
+    let startTime = 60;
+    let time = 0;
+    let timerRunning = false;
+    let timesUp = null;
+
+    function formatTime(time) {
+        let minutes = Math.floor(time / 60);
+        let seconds = time - (minutes * 60);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+    };
+
+    function timerTick() {
+        time--;
+
+        if (typeof displayTime === "function") {
+            displayTime(formatTime(time));
+        }
+
+        if (time <= 0) {
+            timer.stopTimer();
+            time = 0;
+            if (typeof timesUp === "function") {
+                timesUp();
+            }
+        }
+    };
+
+    let timer = {
+        onDisplayTime(displayTimeCallback) {
+            displayTime = displayTimeCallback;
+        },
+
+        onTimesUp(timesUpCallback) {
+            timesUp = timesUpCallback;
+        },
+
+        resetTimer() {
+            time = startTime;
+            if (typeof displayTime === "function") {
+                displayTime(formatTime(time));
+            }
+        },
+
+        startTimer() {
+            if (!timerRunning) {
+                intervalId = setInterval(timerTick.bind(this), 1000);
+                timerRunning = true;
+            }
+        },
+
+        stopTimer() {
+            if (timerRunning) {
+                clearInterval(intervalId);
+                timerRunning = false;
+            }
+        }
+    };
+
+    return timer;
+})();
